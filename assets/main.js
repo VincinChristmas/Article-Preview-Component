3cc9de72a84deb2e917f286163b1629ae89f6cd6
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-13 15:35:13 -0500

    added some javaScript. Almost done

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,47 @@
-const button = document.querySelector('main-footer__share');
+let isRotated = false;
+
+document.getElementById("shareButton").addEventListener("click", function() {
+  var element = document.getElementById("shareButton");
+  var newForm = document.getElementById("main-footer");
+  var newFormImage = document.getElementById('main-footer__img');
+  var name = document.getElementById('main-footer__info');
+  var shareBlock = document.getElementById('main-footer__shareResult');
+  if (isRotated) {
+    element.style.transform = "rotate(45deg)";
+    newForm.style.background = "hsl(217, 19%, 35%)";
+    newFormImage.style.display = "none";
+    name.style.display = "none";
+    shareBlock.style.display = "flex";
+
+  } else {
+    element.style.transform = "rotate(0deg)";
+    newForm.style.background = "white";
+    newFormImage.style.display = "block";
+    name.style.display = "block";
+    shareBlock.style.display = "none";
+
+  }
+  isRotated = !isRotated;
+});
 
 
-button.addEventListener('click', function() {
-    
-}
 
 
 
 
+
+/*
+<div id="myDiv">This is a div.</div>
+<button id="toggleButton">Toggle Div</button>
+
+
+
+document.getElementById("toggleButton").addEventListener("click", function() {
+    var myDiv = document.getElementById("myDiv");
+    if (myDiv.style.display === "none") {
+        myDiv.style.display = "block";
+    } else {
+        myDiv.style.display = "none";
+    }
+});
+*/
