00306e552c53d41cc4fb770cd77ccb9975dcc3b1
VincinChristmas <VincinChristmas@hotmail.com>
2024-10-16 00:48:00 -0500

    made changes to all the code

--- assets/main.js
+++ assets/main.js
@@ -1,35 +1,7 @@
-let isRotated = false;
-let doRotate = false
-
-document.getElementById("shareButton").addEventListener("click", function() {
-  var social = document.getElementById('social');
-  var nonSocial = document.getElementById('non-social');
-  if (isRotated) {
-    nonSocial.style.display = "none";
-    social.style.display = "flex";
-    
-   
-  } else {
-    social.style.display = "none";
-    nonSocial.style.display = "flex";
-  }
-  isRotated = !isRotated;
-});
-
-document.getElementById("shareButtonSocial").addEventListener("click", function() {
-  var social = document.getElementById('social');
-  var nonSocial = document.getElementById('non-social');
-  if (doRotate) {
-    nonSocial.style.display = "flex";
-    social.style.display = "none";
-    
-   
-  } else {
-    social.style.display = "flex";
-    nonSocial.style.display = "none";
-  }
-  doRotate = !doRotate;
-});
-
-
+// Post Share 
+let shareBtn = document.querySelector(".shareBtn");
+shareBtn.addEventListener("click", () => {
+    shareBtn.classList.toggle("active");
+    document.querySelector(".shareMenu").classList.toggle("active")
+})
 
